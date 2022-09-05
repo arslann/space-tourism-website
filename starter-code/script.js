0814eb3e6194f4aa061b5bc13cdb2e47f2bbee06
arslann <arslansahin.1907@gmail.com>
2022-09-05 22:11:55 +0300

    complete destination  page

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,9 +1,47 @@
+let data;
+let json;
+
+const dots = document.querySelectorAll('.dot');
+
+const title = document.getElementById('textbox_title');
+const text = document.getElementById('textbox_text');
+const distance = document.getElementById('distance_km');
+const time = document.getElementById('distance_time');
+const image = document.getElementById('planet_img');
+
 async function loadData() {
-  const data = await fetch('./data.json');
-  const json = await data.json();
+  data = await fetch('./data.json');
+  json = await data.json();
+}
+
+function currentSlide(num) {
   const { destinations, crew, technology } = json;
 
-  console.log(destinations);
+  const selectedPlanet = destinations[num];
+
+  //toggle active class
+  changeActiveClass(num);
+
+  //change title
+  title.innerText = selectedPlanet.name;
+
+  //change description
+  text.innerText = selectedPlanet.description;
+
+  //change distance and travel time
+  distance.innerText = selectedPlanet.distance;
+  time.innerText = selectedPlanet.travel;
+
+  //change planet image
+  image.src = selectedPlanet.images.webp;
+}
+
+function changeActiveClass(num) {
+  dots.forEach((dot) => {
+    dot.classList.remove('active');
+  });
+
+  dots[num].classList.add('active');
 }
 
 loadData();
