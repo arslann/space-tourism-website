4c33e0b08359e4e590b9134718eaba46c758483c
arslann <arslansahin.1907@gmail.com>
2022-09-07 11:46:16 +0300

    complete crew page

--- starter-code/script.js
+++ starter-code/script.js
@@ -3,45 +3,68 @@ let json;
 
 const dots = document.querySelectorAll('.dot');
 
-const title = document.getElementById('textbox_title');
-const text = document.getElementById('textbox_text');
+const title = document.getElementById('descriptionTitle');
+const text = document.getElementById('description');
 const distance = document.getElementById('distance_km');
 const time = document.getElementById('distance_time');
-const image = document.getElementById('planet_img');
+const image = document.getElementById('img');
+const role = document.getElementById('role');
 
 async function loadData() {
   data = await fetch('./data.json');
   json = await data.json();
 }
 
-function currentSlide(num) {
+function currentSlide(num, slide) {
   const { destinations, crew, technology } = json;
+  let selectedSlide;
 
-  const selectedPlanet = destinations[num];
+  switch (slide) {
+    case 'crew':
+      selectedSlide = crew;
+      break;
+    case 'destination':
+      selectedSlide = destinations;
+      break;
+    case 'technology':
+      selectedSlide = technology;
+      break;
+
+    default:
+      break;
+  }
 
   //toggle active class
   changeActiveClass(num);
-
+  console.log(selectedSlide);
   //change title
-  title.innerText = selectedPlanet.name;
+  title.innerText = selectedSlide[num].name;
 
   //change description
-  text.innerText = selectedPlanet.description;
+  text.innerText = selectedSlide[num].description;
 
-  //change distance and travel time
-  distance.innerText = selectedPlanet.distance;
-  time.innerText = selectedPlanet.travel;
+  //change role
+  role.innerText = selectedSlide[num].role;
 
   //change planet image
-  image.src = selectedPlanet.images.webp;
+  image.src = selectedSlide[num].images.webp;
+
+  //change distance and travel time
+
+  if (distance && time) {
+    distance.innerText = selectedSlide[num].distance;
+    time.innerText = selectedSlide[num].travel;
+  }
 }
 
 function changeActiveClass(num) {
   dots.forEach((dot) => {
     dot.classList.remove('active');
+    dot.classList.remove('selectedSlide');
   });
 
   dots[num].classList.add('active');
+  dots[num].classList.add('selectedSlide');
 }
 
 loadData();
